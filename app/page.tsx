42f3908aacbf7dc7381c4dde87543fd168515cde
Эльдар <mimiricksont21229@outlook.com>
2026-05-24 01:25:47 +0300

    remove socials block

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,21 @@
 import Hero from "@/components/Hero";
 import Booking from "@/components/Booking";
 import Reviews from "@/components/Reviews";
-import Socials from "@/components/Socials";
 import Gallery from "@/components/Gallery";
 import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-black text-white">
-
+    <main className="min-h-screen bg-black">
       <Hero />
 
       <Booking />
 
       <Reviews />
 
-      <Socials />
-
       <Gallery />
 
       <Footer />
-
     </main>
   );
 }
